7c9e0168c0d64c30ade3f31496b736ad921e1ea2
Liz Burton <liz.burton147@gmail.com>
2021-11-01 08:58:33 -0400

    add solution

--- lib/tree.rb
+++ lib/tree.rb
@@ -3,4 +3,14 @@ class Tree
     @root = root
   end
 
+  def get_element_by_id(id)
+    holder = [@root]
+
+    while (holder.length > 0)
+      current = holder.pop
+      return current if current[:value] == id
+      holder.push(*current[:children])
+    end
+    nil
+  end
 end
