d604cc30cbc4a1b9e1b1c659852524a72dee5fde
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-12 11:25:01 -0400

    commit tree 6aeb1866d0197a3e2408395c0cfd40617c5a45ce

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,16 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(str)
+    if @root[:id] == str
+      return @root
+    else
+      @root[:children].each do |child|
+        return Tree.new(child).get_element_by_id(str)
+      end
+    end
+    return nil
+  end
+
 end
